daced0f008e5ea36d6d17f8dc2b7c2e63e73fe39
Martin Mbithi <martdevelopers254@gmail.com>
2022-01-04 13:56:44 +0300

    Inject Alerts - Swal Mixins

--- partials/scripts.php
+++ partials/scripts.php
@@ -4,3 +4,52 @@
  <script src="../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
  <!-- AdminLTE App -->
  <script src="../public/js/adminlte.min.js"></script>
+ <!-- SweetAlert2 -->
+ <script src="../public/plugins/sweetalert2/sweetalert2.min.js"></script>
+ <!-- Init Sweet Alerts -->
+ <?php if (isset($success)) { ?>
+     <script>
+         const Toast = Swal.mixin({
+             toast: true,
+             position: 'top-end',
+             showConfirmButton: false,
+             timer: 3000
+         });
+         Toast.fire({
+             type: 'success',
+             title: '<?php echo $success; ?>',
+         })
+     </script>
+
+ <?php }
+    if (isset($err)) { ?>
+     <script>
+         const Toast = Swal.mixin({
+             toast: true,
+             position: 'top-end',
+             showConfirmButton: false,
+             timer: 3000
+         });
+         Toast.fire({
+             type: 'error',
+             title: '<?php echo $err; ?>',
+         })
+     </script>
+
+ <?php }
+    if (isset($info)) { ?>
+     <script>
+         const Toast = Swal.mixin({
+             toast: true,
+             position: 'top-end',
+             showConfirmButton: false,
+             timer: 3000
+         });
+         Toast.fire({
+             type: 'info',
+             title: '<?php echo $info; ?>',
+         })
+     </script>
+
+ <?php }
+    ?>
